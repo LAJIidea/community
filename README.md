1bea5f93c8aeb82eceba1ef2e846b18950fdc001
kingnights <864970002@qq.com>
2019-10-21 15:05:33 +0800

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+##kingkiller
+
+##资料
+[spring 文档](https://spring.io/guides)
+[spring web](https://spring.io/guides/gs/serving-web-content/)
+[es](https://elasticsearch.cn/explore)
+[Github添加key](https://developer.github.com/v3/guides/managing-deploy-keys/)
+[Bootstrap](https://v3.bootcss.com/)
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+[spring内置数据库](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
+
+##工具
+[Git](https://git-scm.com/download/win)
+[vs](https://www.visual-paradigm.com)
--- src/main/java/cn/kingkiller/community/AuthorizeController.java
+++ src/main/java/cn/kingkiller/community/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -25,7 +27,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AcesstokenDTO acesstokenDTO = new AcesstokenDTO();
         acesstokenDTO.setClient_id(clientId);
         acesstokenDTO.setClient_secret(clientSecret);
@@ -34,7 +37,14 @@ public class AuthorizeController {
         acesstokenDTO.setState(state);
         String acessToken = githubProvide.getAcessToken(acesstokenDTO);
         GithubUser user = githubProvide.githubUser(acessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user !=null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
